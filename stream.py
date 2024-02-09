3279341d0726f03b9516fed2535a6e1d2647370d
unknown <tkiely99@gmail.com>
2024-02-08 21:28:32 -0500

    update env file and output source mp3

--- stream.py
+++ stream.py
@@ -1,12 +1,16 @@
 from pathlib import Path
 from openai import OpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
 client = OpenAI()
 
-speech_file_path = Path(__file__).parent / "speech.mp3"
+#speech_file_path = Path(__file__).parent / "speech.mp3"
 response = client.audio.speech.create(
   model="tts-1",
   voice="onyx",
   input="Today is a wonderful day to build something people love!"
 )
 
-response.stream_to_file(speech_file_path)
+response.with_streaming_response.method("output.mp3")
