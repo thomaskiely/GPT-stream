3bbefb3d2cdb186b8d717465572c143eb3ce8bf6
unknown <tkiely99@gmail.com>
2024-02-10 21:19:44 -0500

    pygame stuff

--- stream.py
+++ stream.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 import irc.bot
+import pygame
 load_dotenv()
 
 
@@ -11,9 +12,7 @@ class TwitchChatBot(irc.bot.SingleServerIRCBot):
     server = 'irc.chat.twitch.tv'
     port = 6667
     irc.bot.SingleServerIRCBot.__init__(self, [(server, port, 'oauth:' + token)], username, username)
-    
-  
-  print("a")
+
  
   def on_welcome(self, connection, event):
        
@@ -28,8 +27,24 @@ class TwitchChatBot(irc.bot.SingleServerIRCBot):
                 username = event.source.split('!')[0]
                 message = event.arguments[0]
                 print(f'{username}: {message}')
+                self.callOpenAI()
         except Exception as e:
             print("Error in on_pubmsg:", e)
+
+  def callOpenAI(self):
+      print("TODO: call OPEN AI with message")
+      self.drawSam()
+
+  def drawSam(self):
+     
+      #FIX THIS
+      screen = pygame.display.set_mode((800, 800))
+      talkingSam = pygame.image.load('samTalking.png')
+      notTalkingSam = pygame.image.load('samNotTalking.png')
+      screen.blit(talkingSam,(200,200))
+      pygame.display.update()
+      pygame.event.pump()
+      print("Draw Samantha")
 
 
 
@@ -39,5 +54,6 @@ if __name__ == "__main__":
   TWITCH_CLIENT_ID= os.getenv('TWITCH_CLIENT_ID')
   TWITCH_USERNAME= os.getenv('TWITCH_USERNAME')
   CHANNEL = '#' + TWITCH_USERNAME
+  pygame.init()
   bot = TwitchChatBot(TWITCH_USERNAME, TWITCH_TOKEN, CHANNEL)
   bot.start()
